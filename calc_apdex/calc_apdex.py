73211af2e1d2015f01ae1739b7a00414d9e9d946
martiroman <martinnahuel.roman@protonmail.com>
2023-05-15 14:54:51 -0300

    v1.3

--- calc_apdex/calc_apdex.py
+++ calc_apdex/calc_apdex.py
@@ -1,9 +1,8 @@
-import requests
 from ruxit.api.base_plugin import RemoteBasePlugin
 import logging
-from math import floor
-from datetime import datetime
-
+import requests
+from datetime import datetime,timedelta
+from croniter import croniter
 import pandas as pd
 import numpy as np
 import urllib.parse
@@ -11,39 +10,53 @@ import urllib.parse
 logger = logging.getLogger(__name__)
 
 class ApdexPluginRemote(RemoteBasePlugin):
-    def __init__(self, **kwargs):
-        
-        super().__init__(**kwargs)
-        self.start_time=floor(datetime.now().timestamp()*1000) - self.pollingInterval
-        self.end_time=None
 
     def initialize(self, **kwargs):
         self.name = self.config["name"]
         self.metric = self.config["metric"]
-        self.pollingInterval = self.config["pollingInterval"]
+        self.token = self.config["token"]
+        self.endpoint = self.config["endpoint"]
+        self.schedule = self.config["schedule"] or "*/1 * * * *"
+        self.timeframe = self.config["timeframe"] or "now-24h"
+        #self.rango = self.config["rango"] or "8-20"
+
+    def sendMetric(self, val):
+        parametros = {
+            'accept': 'application/json; charset=utf-8',
+            'Authorization': 'Api-Token ' + self.token,
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
+
+        data = 'apdex.horalab,app=' + self.name +' ' + str(val)
+        query = "/v2/metrics/ingest?"
+
+        respuesta = requests.post(self.endpoint + query, headers=parametros, data=data, verify=False)
+
+        if respuesta.status_code < 300:
+            logger.info("Metrica enviada")
+        else:
+            logger.info("Error al enviar la metrica. Codigo de estado: ", respuesta.status_code)
 
     def getMetric(self):
 
-        TIMEFRAME = "&from=now-7d"
+        TIMEFRAME = "&from=" + self.timeframe
         RESOLUTION = "&resolution=1m"
-        METRIC = self.metric
-
-        QUERY = "/v2/metrics/query?metricSelector=" + urllib.parse.quote(METRIC, safe='()') + RESOLUTION  + TIMEFRAME
-
+        metric = self.metric
+        query = "/v2/metrics/query?metricSelector=" + urllib.parse.quote(metric, safe='()') + RESOLUTION  + TIMEFRAME
 
         parametros = {
             'accept': 'application/json; charset=utf-8',
-            'Authorization': 'Api-Token ' + API_TOKEN
+            'Authorization': 'Api-Token ' + self.token
         }
 
-        respuesta = requests.get(API_URL + QUERY, headers=parametros, verify=False)
+        respuesta = requests.get(self.endpoint + query, headers=parametros, verify=False)
+
         if respuesta.status_code == 200:
             json_data = respuesta.json()
         else:
-            print('Error al hacer la solicitud a la API. Código de estado: ', respuesta.status_code)
+            logger.info("Error al hacer la solicitud API. Codigo de estado: ", respuesta.status_code)
 
         metricId = json_data["result"][0]["metricId"]
-
         dimensionMap = json_data["result"][0]["data"][0]["dimensionMap"]
         timestamps = json_data["result"][0]["data"][0]["timestamps"]
         values = json_data["result"][0]["data"][0]["values"]
@@ -53,21 +66,16 @@ class ApdexPluginRemote(RemoteBasePlugin):
         # Elimino las columnas sin valor (None)
         DF_Data.dropna(axis=0, inplace=True)
 
-        # Considera solo LaV de 8.00 a 20.00hs
 
         DF_Data['Timestamp'] = pd.to_datetime(DF_Data['Timestamp'], unit='ms')
         filtro = ((DF_Data['Timestamp'].dt.dayofweek >= 0) & (DF_Data['Timestamp'].dt.dayofweek <= 4)) & ((DF_Data['Timestamp'].dt.hour >= 8) & (DF_Data['Timestamp'].dt.hour <= 19))
 
         DF_Data = DF_Data[filtro]
-        #DF_Data.to_csv('datos.csv', index=False)
 
         X = np.array(DF_Data["Value"], dtype=float)
 
         # Redondeo de decimales
-        X = np.around(X, decimals=3)
-
-        #moda = stat.mode(X)
-        #moda_cont = np.count_nonzero(X == moda)
+        #X = np.around(X, decimals=3)
 
         # Calcular los valores estadísticos
         p10 = np.percentile(X, 10)
@@ -84,18 +92,15 @@ class ApdexPluginRemote(RemoteBasePlugin):
         '''
         Routine call from the ActiveGate
         '''
+        now = datetime.now().astimezone()
+        cron = croniter(self.schedule, now, ret_type=datetime)
+        previous_execution = cron.get_prev().astimezone()
+        next_execution = cron.get_next().astimezone()
 
-        group = self.topology_builder.create_group(identifier="ApdexGroup", group_name="APDEX")
-        device = group.create_device(identifier=self.name, display_name="APDEX")
-        logger.info("Topology: group name=%s, device name=%s", group.name, device.name)
-        
-        self.end_time = floor(datetime.now().timestamp()*1000)
-        if self.end_time - self.start_time >= self.pollingInterval:
-            val = self.getMetric()  
-            self.start_time = self.end_time + 1
-            device.absolute(key='apdex', value=val, dimensions = { "name" : self.name })
-            logger.info("APDEX" + " | " + self.name + " = " + str(val))
+        time_since = now - previous_execution
+        if time_since <= timedelta(minutes=1):
+            val = self.getMetric()
 
-        
-        ## Metadata:
-        #device.report_property(key='IP addresses', value=self.ipservidor)
+            self.sendMetric(val)
+
+            logger.info(now.strftime("%Y-%M-%d %H:%M:%S") + " : APDEX  | " + self.name + " = " + str(val))
